db0023131c39f64a689e349142a6d97f7cce9a45
eternal198 <ikuya313198@gmail.com>
2021-05-20 18:30:08 +0900

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,16 @@
-// Hello
-(() => {
-  const greeting = 'hello';
-  console.log(greeting);
-})();
+const config = {
+  apiKey: "AIzaSyCXkqAFgbz3hP6LLpQt11iJzXcXrc8MDgU",
+  authDomain: "mealticket-1585f.firebaseapp.com",
+  databaseURL: "https://mealticket-1585f-default-rtdb.firebaseio.com",
+  projectId: "mealticket-1585f",
+  storageBucket: "mealticket-1585f.appspot.com",
+  messagingSenderId: "171132223510",
+  appId: "1:171132223510:web:13ea0883b7e66187893537",
+  measurementId: "G-MS6L1K17ED"
+};
+
+firebase.initializeApp(config);
+
+const database = firebase.database();
 
 
-// good morning
-(() => {
-  const greeting = "good morning";
-  console.log(greeting);
-})();
