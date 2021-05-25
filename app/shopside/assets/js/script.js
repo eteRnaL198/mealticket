ea392348d32054548763ed7e21c42f67825cbd46
eternal198 <ikuya313198@gmail.com>
2021-05-25 23:00:09 +0900

    update

--- app/shopside/assets/js/script.js
+++ app/shopside/assets/js/script.js
@@ -10,3 +10,54 @@ const config = {
 };
 firebase.initializeApp(config);
 
+firebase.database().ref('tickets/menu').once('value', snapshot => {
+  const data = snapshot.val();
+  const buttons = data.map((menu, idx) => {
+    const inner = document.getElementById('js-menuInner');
+    const button = document.createElement('button');
+    button.classList.add('menu_button');
+    button.innerHTML = menu.name;
+    button.addEventListener('click', () => {
+      displayTicket(menu.name, idx);
+    })
+    inner.insertAdjacentElement("beforeend", button);
+  });
+  const toggleButton = (thisButton) => {
+    buttons.forEach(button, () => {
+      button.classList.remove('pressed');
+    });
+    thisButton.classList.add('pressed');
+  }
+});
+
+const displayTicket = (name, idx) => {
+  const inner = document.getElementById('js-ticketInner');
+  while(inner.firstChild){
+    inner.removeChild(inner.firstChild);
+  }
+  const title = document.getElementById('js-ticketTitle');
+  title.innerHTML = name;
+  firebase.database().ref('tickets/menu/'+idx+'/issued').on('value', snapshot => {
+    const issued = snapshot.val();
+    if(issued.length > 1) {
+      issued.forEach((val,idx) => {
+      if(idx !== 0) {
+        const inner = document.getElementById('js-ticketInner');
+        const card = document.createElement('div');
+        const codeText = document.createElement('p');
+        const code = document.createElement('p');
+        const num = document.createElement('p');
+        codeText.innerHTML = '受け取りコード: ';
+        code.classList.add('ticket_code');
+        code.innerHTML = val.code;
+        num.innerHTML = '確認番号: ' + val.auth_num;
+        card.classList.add('ticket_card');
+        card.insertAdjacentElement("beforeend", codeText);
+        card.insertAdjacentElement("beforeend", code);
+        card.insertAdjacentElement("beforeend", num);
+        inner.insertAdjacentElement("beforeend",card);
+      }
+    })
+  }
+  });
+}
