af7bf5c5085e2573dfec656a5585af1e9ad53319
eternal198 <ikuya313198@gmail.com>
2021-05-25 20:59:32 +0900

    update

--- app/customerside/assets/js/data.js
+++ app/customerside/assets/js/data.js
@@ -1,208 +1,191 @@
-// const config = {
-//   apiKey: "AIzaSyCXkqAFgbz3hP6LLpQt11iJzXcXrc8MDgU",
-//   authDomain: "mealticket-1585f.firebaseapp.com",
-//   databaseURL: "https://mealticket-1585f-default-rtdb.firebaseio.com",
-//   projectId: "mealticket-1585f",
-//   storageBucket: "mealticket-1585f.appspot.com",
-//   messagingSenderId: "171132223510",
-//   appId: "1:171132223510:web:13ea0883b7e66187893537",
-//   measurementId: "G-MS6L1K17ED"
-// };
-
-// firebase.initializeApp(config);
-
-
-
-const writeData = () => {
-  menus.map((menu, idx) => {
-    firebase.database().ref('tickets/' + idx).set({
-      menu
-    });
-  });
-}
-// writeData();
-
 // menuRef.once('value', snapshot => {
 //   const data = snapshot.val();
 //   console.log(data);
 // })
-const menu = firebase.database().ref('tickets/menu/3');
-menu.once('value', snapshot => {
-  const data = snapshot.val();
-  // console.log(data);
-})
+const issued = [{code: "abc123", auth_num: 0}];
+const newTicket = {code: "def456", auth_num: 1};
+const rewriteMenu = () => {
+  firebase.database().ref('tickets/menu/' + 0 + '/issued').set(
+    [{dummy: "hoge"}]
+  );
+}
+// rewriteMenu();
 
 
 
-const menus = [
+const menu = [
   {
     name: "唐揚げ定食",
-    issued: 0,
+    issued: [{dummy:"hoge"}],
     price: 500
   },
   {
     name: "唐揚げ弁当",
-    issued: 0,
+    issued: [{dummy:"hoge"}],
     price: 500
   },
   {
     name: "デイリーランチ",
-    issued: 0,
+    issued: [{dummy:"hoge"}],
     price: 370
   },
   {
     name: "デイリーランチライス小",
-    issued: 0,
+    issued: [{dummy:"hoge"}],
     price: 350
   },
   {
     name: "デイリーランチライス大",
-    issued: 0,
+    issued: [{dummy:"hoge"}],
     price: 420
   },
   {
     name: "唐揚げ丼L",
-    issued: 0,
+    issued: [{dummy:"hoge"}],
     price: 470
   },
   {
     name: "唐揚げ丼M",
-    issued: 0,
+    issued: [{dummy:"hoge"}],
     price: 390
   },
   {
     name: "カレーライス",
-    issued: 0,
+    issued: [{dummy:"hoge"}],
     price: 310
   },
   {
     name: "カレー大盛",
-    issued: 0,
+    issued: [{dummy:"hoge"}],
     price: 360
   },
   {
     name: "ミニカレー",
-    issued: 0,
+    issued: [{dummy:"hoge"}],
     price: 160
   },
   {
     name: "生卵",
-    issued: 0,
+    issued: [{dummy:"hoge"}],
     price: 50
   },
   {
     name: "温泉卵",
-    issued: 0,
+    issued: [{dummy:"hoge"}],
     price: 50
   },
   {
     name: "チーズ",
-    issued: 0,
+    issued: [{dummy:"hoge"}],
     price: 50
   },
   {
     name: "コロッケ",
-    issued: 0,
+    issued: [{dummy:"hoge"}],
     price: 50
   },
   {
     name: "日替わりトッピング",
-    issued: 0,
+    issued: [{dummy:"hoge"}],
     price: 80
   },
   {
     name: "特別中華麺",
-    issued: 0,
+    issued: [{dummy:"hoge"}],
     price: 540
   },
   {
     name: "日替わり中華麺",
-    issued: 0,
+    issued: [{dummy:"hoge"}],
     price: 420
   },
   {
     name: "特別麺そば",
-    issued: 0,
+    issued: [{dummy:"hoge"}],
     price: 540
   },
   {
     name: "特別麺うどん",
-    issued: 0,
+    issued: [{dummy:"hoge"}],
     price: 540
   },
   {
     name: "日替わりそば",
-    issued: 0,
+    issued: [{dummy:"hoge"}],
     price: 300
   },
   {
     name: "日替わりうどん",
-    issued: 0,
+    issued: [{dummy:"hoge"}],
     price: 300
   },
   {
     name: "麺大盛+半玉",
-    issued: 0,
+    issued: [{dummy:"hoge"}],
     price: 50
   },
   {
     name: "麺ジャンボ+1玉",
-    issued: 0,
+    issued: [{dummy:"hoge"}],
     price: 100
   },
   {
     name: "きつね",
-    issued: 0,
+    issued: [{dummy:"hoge"}],
     price: 30
   },
   {
     name: "たぬき",
-    issued: 0,
+    issued: [{dummy:"hoge"}],
     price: 30
   },
   {
     name: "竹輪天",
-    issued: 0,
+    issued: [{dummy:"hoge"}],
     price: 50
   },
   {
     name: "カレーソース",
-    issued: 0,
+    issued: [{dummy:"hoge"}],
     price: 90
   },
   {
     name: "かき揚げ",
-    issued: 0,
+    issued: [{dummy:"hoge"}],
     price: 50
   },
   {
     name: "小鉢サラダ",
-    issued: 0,
+    issued: [{dummy:"hoge"}],
     price: 80
   },
   {
     name: "ライス大",
-    issued: 0,
+    issued: [{dummy:"hoge"}],
     price: 150
   },
   {
     name: "ライス中",
-    issued: 0,
+    issued: [{dummy:"hoge"}],
     price: 100
   },
   {
     name: "ライス小",
-    issued: 0,
+    issued: [{dummy:"hoge"}],
     price: 80
   },
   {
     name: "味噌汁",
-    issued: 0,
+    issued: [{dummy:"hoge"}],
     price: 30
   },
   {
     name: "ソフトクリーム",
-    issued: 0,
+    issued: [{dummy:"hoge"}],
     price: 200
   }
 ]
+const setMenu = () => {
+  firebase.database().ref('tickets/').update({menu});
+}
+// setMenu();
--- app/customerside/assets/js/script.js
+++ app/customerside/assets/js/script.js
@@ -10,6 +10,7 @@ const config = {
 };
 firebase.initializeApp(config);
 
+
 // メニュー表示
 const menuRef = firebase.database().ref('tickets/menu');
 menuRef.once('value', snapshot => {
@@ -23,7 +24,7 @@ menuRef.once('value', snapshot => {
     name.classList.add("menuCard_name");
     name.innerHTML = data[i].name;
     number.classList.add("menuCard_number");
-    number.innerHTML = "未受取数: " + data[i].issued;
+    number.innerHTML = "未受取数: " + data[i].issued.length;
     price.classList.add("menuCard_price");
     price.innerHTML = data[i].price + "円";
     card.classList.add("menuCard");
@@ -36,13 +37,12 @@ menuRef.once('value', snapshot => {
     card.addEventListener('click', () => {
       displayDetail([data[i].name, data[i].issued, data[i].price, i]);
     });
-    firebase.database().ref('tickets/menu/'+i).on('value', snapshot => {
+    firebase.database().ref('tickets/menu/'+i+'/issued').on('value', snapshot => {
       const data = snapshot.val();
-      number.innerHTML = "未受取数: " + data.issued;
+      number.innerHTML = "未受取数: " + --data.length;
     });
   }
 });
-
 
 
 // メニュー詳細表示
@@ -61,7 +61,7 @@ const displayDetail = ([arg_name, arg_issued, arg_price, arg_idx]) => {
   name.classList.add("menuDetail_name");
   name.innerHTML = arg_name;
   num.classList.add("menuDetail_num");
-  num.innerHTML = "未受取人数" + arg_issued;
+  num.innerHTML = "未受取人数" + arg_issued.length;
   price.classList.add("menuDetail_price");
   price.innerHTML = arg_price + "円";
   orderButton.classList.add("menuDetail_order");
@@ -80,29 +80,34 @@ const displayDetail = ([arg_name, arg_issued, arg_price, arg_idx]) => {
   orderButton.addEventListener('click', () => {
     const code = Math.random().toString(32).substring(2,9);
     const auth_num = Math.floor((Math.random() * 9) + 1);
+    const newIssued = {code: code, auth_num: auth_num};
     console.log("code: "+code+", auth_num: "+ auth_num);
-    // 確認コード，受け取り番号をデータベースに追加
-    displayTicket([arg_name, arg_issued, arg_price, arg_idx, code, auth_num]);
-    firebase.database().ref('tickets/menu/'+arg_idx).once('value', snapshot => {
+    displayTicket([arg_name, arg_idx, code, auth_num]);
+    firebase.database().ref('tickets/menu/' + arg_idx + '/issued').once('value', snapshot => {
       const data = snapshot.val();
-      arg_issued = data.issued;
+      arg_issued = data;
     });
-    firebase.database().ref('tickets/menu/' + arg_idx).update(
-      {
-        name: arg_name,
-        issued: ++arg_issued,
-        price: arg_price,
-      }
+    firebase.database().ref('tickets/menu/' + arg_idx + '/issued').set(
+        [...arg_issued, newIssued]
     );
+    const orderedTickets = JSON.parse(localStorage.getItem('tickets'));
+    const newTicket = {
+      name: arg_name,
+      idx: arg_idx,
+      code: code,
+      auth_num: auth_num
+    };
+    const newOrderedTickets = (orderedTickets !== null) ? [...orderedTickets, newTicket] : [newTicket];
+    localStorage.setItem('tickets', JSON.stringify(newOrderedTickets));
   });
-  firebase.database().ref('tickets/menu/'+arg_idx).on('value', snapshot => {
+  firebase.database().ref('tickets/menu/'+arg_idx+'/issued').on('value', snapshot => {
     const data = snapshot.val();
-    num.innerHTML = "未受取数: " + data.issued;
+    num.innerHTML = "未受取数: " + --data.length
   });
 }
 
 //食券表示
-const displayTicket = ([arg_name, arg_issued, arg_price, arg_idx, arg_code, arg_auth_num]) => {
+const displayTicket = ([arg_name, arg_idx, arg_code, arg_auth_num]) => {
   const back = document.getElementById('js-backScreen');
   const modal = document.getElementById('js-menuDetail');
   const inner = document.getElementById('js-ordered_inner');
@@ -124,13 +129,22 @@ const displayTicket = ([arg_name, arg_issued, arg_price, arg_idx, arg_code, arg_
     modal.remove();
   }
   button.addEventListener("click", () => 
-    displayReserve([arg_name, arg_issued, arg_price, arg_idx, arg_code, arg_auth_num, card])
+    displayReserve([arg_name, arg_idx, arg_code, arg_auth_num, card])
   );
 }
+(() => {
+  const tickets = JSON.parse(localStorage.getItem('tickets'));
+  console.log(tickets);
+  if(tickets !== null) {
+    tickets.map((ticket) => {
+      displayTicket([ticket.name, ticket.idx, ticket.code, ticket.auth_num]);
+    });
+  }
+})();
 
 
 //受け取り
-const displayReserve = ([arg_name, arg_issued, arg_price, arg_idx, arg_code, arg_auth_num, arg_elem]) => {
+const displayReserve = ([arg_name, arg_idx, arg_code, arg_auth_num, arg_elem]) => {
   const wrapper = document.getElementById("js-ordered_wrapper");
   const orderedInner = document.getElementById("js-ordered_inner");
   const none = document.getElementById("js-ordered_none");
@@ -191,18 +205,19 @@ const displayReserve = ([arg_name, arg_issued, arg_price, arg_idx, arg_code, arg
       arg_elem.remove();
       reserve.remove();
       if(orderedInner.children.length === 1) none.classList.remove("hidden");
+      let issued;
+      firebase.database().ref('tickets/menu/'+ arg_idx + '/issued').once('value', snapshot => {
+        const data = snapshot.val();
+        issued = data;
+        issued.pop();
+      });
+      firebase.database().ref('tickets/menu/' + arg_idx + '/issued').set(
+        issued
+      );
+      const orderedTickets = JSON.parse(localStorage.getItem('tickets'));
+      const newOrderedTickets = orderedTickets.filter(ticket => ticket.code !== arg_code)
+      localStorage.setItem('tickets', JSON.stringify(newOrderedTickets));
     };
-    firebase.database().ref('tickets/menu/'+arg_idx).once('value', snapshot => {
-      const data = snapshot.val();
-      arg_issued = data.issued;
-    });
-    firebase.database().ref('tickets/menu/' + arg_idx).update(
-      {
-        name: arg_name,
-        issued: --arg_issued,
-        price: arg_price,
-      }
-    );
   });
 }
 
